598a534c061e9d933097286f6558f972f990dff9
GitHub Action <action@github.com>
2023-08-28 05:29:04 +0000

    Template cleanup

--- src/main/kotlin/com/github/aabbdev29/darkgreenjungle/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/aabbdev29/darkgreenjungle/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.aabbdev29.darkgreenjungle.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
